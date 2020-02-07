ed4d06f5837ac36b48f7e02ff333df8e98c5da0b
David Cote <davesdere@users.noreply.github.com>
2020-02-06 23:49:05 -0500

    Create file_system_watcher.ps1

---
+++ file_system_watcher.ps1
@@ -0,0 +1,61 @@
+$FileSystemWatcherParams = @{
+
+  Path = 'C:\Users\Proxb\Desktop\Dropbox'
+
+  Recurse =  $True
+
+  NotifyFilter =  'FileName'
+
+  Verbose =  $True
+
+  Action=  {
+
+  $Item  = Get-Item $Event.SourceEventArgs.FullPath
+
+  $WriteHostParams  = @{
+
+  ForegroundColor = 'Green'
+
+  BackgroundColor = 'Black'
+
+  }            
+
+  Switch  -regex ($Item.Extension) {
+
+  '\.(ps1|psm1|psd1)'  {$WriteHostParams.Object  = "Processing  PowerShell file: $($Item.Name)"}
+
+  '\.(docx|doc)'  {$WriteHostParams.Object  = "Processing  Word document: $($Item.Name)"}
+
+  '\.(xlsx|xls)'  {$WriteHostParams.Object  = "Processing  Excel spreadsheet: $($Item.Name)"}
+
+  '\.csv'  {$WriteHostParams.Object  = "Processing  CSV spreadsheet: $($Item.Name)"}
+
+  '\.xml'  {$WriteHostParams.Object  = "Processing  XML document: $($Item.Name)"}
+
+  '\.exe'  {$WriteHostParams.Object  = "Processing  Executable: $($Item.Name)"}
+
+  '\.onepkg'  {$WriteHostParams.Object  = "Processing  OneNote package: $($Item.Name)"}
+
+  '\.lnk'  {$WriteHostParams.Object  = "Processing  Link: $($Item.Name)"}
+
+  '\.cer|\.pfx'  {$WriteHostParams.Object  = "Processing  Certificate File: $($Item.Name)"}
+
+  Default{$WriteHostParams.Object  = "Processing  File: $($Item.Name)"}
+
+  }   
+
+  $Item  | Remove-Item
+
+  Write-Host  @WriteHostParams 
+
+  }
+
+}
+@('Created') | ForEach {
+
+  $FileSystemWatcherParams.EventName = $_
+
+  Start-FileSystemWatcher  @FileSystemWatcherParams
+
+}
+
